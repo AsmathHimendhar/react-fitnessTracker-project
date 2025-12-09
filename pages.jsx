5fe5064f0ad7d736233ac14c21273f597434149b
AsmathHimendhar <103575371+AsmathHimendhar@users.noreply.github.com>
2025-12-09 17:35:07 +0530

    Update pages.jsx

--- pages.jsx
+++ pages.jsx
@@ -1,23 +1,37 @@
-import { memo } from "react";
-import  { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-const Header = memo(({setValid}) => {
-    console.log("Header rendered");
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./login";
+import Dashboard from "./dashboard";
+import ProtectedRoute from "./protector";
+import Meals from "./meals";
+import  Workouts from "./workouts";
+const Pages = React.memo(({valid,setValid}) => {
+    
+    console.log("Pages rendered");
     const[count,setcount]=useState(0);
-     const location = useLocation();
-    const currentPath = location.pathname;
-    console.log(currentPath,"**********");
     return (
-        <header style={{ borderBottom: '1px solid #ccc', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <h1 style={{ margin: '10px' }}>Fitness Tracker</h1>
-             <button onClick={() => {setcount(c=>c+1)}}>header</button>
-            {currentPath!=="/" ?(<nav style={{ margin: "10px",display: 'flex', gap: '20px' }}>
-                <Link to="/" onClick={() => setValid(false)}>Login</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/meals">Meals</Link>
-                <Link to="/workout">Workout</Link>
-            </nav>):(<p>login</p>)}
-        </header>
+        <div>
+            <button onClick={() => setcount(c=>c+1)}>pages</button>
+            {<Routes>
+            <Route path="/" element={<Login setValid={setValid} />} />
+            <Route path="/dashboard" element={
+        <ProtectedRoute valid={valid}>
+            <Dashboard />
+        </ProtectedRoute>}
+            />
+            <Route path="/workout" element={
+        <ProtectedRoute valid={valid}>
+            <Workouts/>
+        </ProtectedRoute>}
+            />
+            <Route path="/meals" element={
+        <ProtectedRoute valid={valid}>
+            <Meals />
+        </ProtectedRoute>}
+            />
+
+        </Routes> }
+        </div>
     );
 })
-export default Header;
+export default Pages;
